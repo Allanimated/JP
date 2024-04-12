9e13d53bfa81ae0ea560734e5f80f86fa5027b1f
Allan Njoroge <allcreativewrites@gmail.com>
2024-04-12 17:42:57 +0300

    feat: Added Data Reveal animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const loader = document.getElementById("loader");
+"use strict";
 
+const loader = document.getElementById("loader");
 
 document.addEventListener("DOMContentLoaded", () => {
   const navMenuIcon = document.querySelector(".w-icon-nav-menu");
@@ -9,15 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const resourcesNav = document.querySelector(".nav-dropdown-list");
   const navBarLinks = document.querySelectorAll(".navbar-links");
 
- //loader
+  //loader
   loader.classList.add("loaded");
   document.body.classList.remove("active");
 
   const toggleNav = (event) => {
     if (nav.style.display === "none" || nav.style.display === "") {
       nav.style.display = "flex";
-      icon.innerText = "close"
-
+      icon.innerText = "close";
     } else {
       nav.style.display = "none";
       icon.innerText = "menu";
@@ -25,24 +25,48 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const toggleResources = (event) => {
-    if (resourcesNav.style.display === "none" || resourcesNav.style.display === "") {
+    if (
+      resourcesNav.style.display === "none" ||
+      resourcesNav.style.display === ""
+    ) {
       resourcesNav.style.display = "flex";
     } else {
       resourcesNav.style.display = "none";
     }
-  }
+  };
 
   navMenuIcon.addEventListener("click", toggleNav);
   navDropdown.addEventListener("click", toggleResources);
   navBarLinks.forEach((navLink) =>
-    navLink.addEventListener("click", (event)=>{
+    navLink.addEventListener("click", (event) => {
       console.log(window.innerWidth);
       if (window.innerWidth <= 991) {
-        toggleNav(event)
+        toggleNav(event);
       }
     })
   ); // close navbar when link is clicked only in tablet and phone view
 
+  /**
+   * SCROLL REVEAL
+   */
 
+  const revealElements = document.querySelectorAll("[data-reveal]");
+
+  const scrollReveal = function () {
+    for (let i = 0; i < revealElements.length; i++) {
+      const elementIsInScreen =
+        revealElements[i].getBoundingClientRect().top <
+        window.innerHeight / 1.15;
+
+      if (elementIsInScreen) {
+        revealElements[i].classList.add("revealed");
+      } else {
+        revealElements[i].classList.remove("revealed");
+      }
+    }
+  };
+
+  window.addEventListener("scroll", scrollReveal);
+
+  scrollReveal();
 });
-
